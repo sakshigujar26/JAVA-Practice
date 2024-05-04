2ada225c637dfe6b9c736936e2929affd07ef2fd
sakshigujar26 <2022bit@sggs.ac.in>
2024-05-04 23:50:35 +0530

    javaPractice

---
+++ packageEx1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class PackageExample{
+	public static void main(String args[]){
+		System.out.println("hi...");
+		Scanner sc = new Scanner(System.in);
+		sc.nextInt();
+	
+}
+}
---
+++ packageEx2.java
@@ -0,0 +1,9 @@
+
+//without import we can use path for input with class scanner
+class PackageExample1{
+	public static void main(String args[]){
+		System.out.println("hi");
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+		sc.nextInt();
+		}
+		}
